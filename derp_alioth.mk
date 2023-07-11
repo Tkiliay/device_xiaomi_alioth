d35b685559ecc6fe8aa45a62ee848f2a4727f3f3
Tkiliay <sackclouths@outlook.com>
2023-07-11 21:22:55 +0800

    alioth: Redmi K40
    i prefer Redmi k40

--- derp_alioth.mk
+++ derp_alioth.mk
@@ -17,9 +17,9 @@ $(call inherit-product, device/xiaomi/alioth/device.mk)
 PRODUCT_NAME := derp_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_BRAND := POCO
-PRODUCT_MODEL := POCO F3
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi K40
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := POCO/alioth_global/alioth:13/TKQ1.220829.002/V14.0.4.0.TKHMIXM:user/release-keys
+BUILD_FINGERPRINT := Redmi/alioth/alioth:13/TKQ1.220829.002/V14.0.4.0.TKHCNXM:user/release-keys
