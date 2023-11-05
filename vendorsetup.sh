96d8993f08d27d83990218905ded2515b9724f01
Tkiliay <sackclouths@outlook.com>
2023-11-05 12:44:10 +0800

    alioth: vendorsetup: Clone hardware/xiaomi

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,3 +3,6 @@ git clone -b 14 https://github.com/Tkiliay/device_xiaomi_sm8250-common device/xi
 git clone -b upstream https://github.com/Tkiliay/android_kernel_xiaomi_sm8250 kernel/xiaomi/sm8250
 git clone -b lineage-20 https://github.com/Tkiliay/proprietary_vendor_xiaomi_alioth vendor/xiaomi/alioth
 git clone -b lineage-20 https://github.com/Tkiliay/proprietary_vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common
+
+git clone -b lineage-21 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
+
